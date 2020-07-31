669fbeb5a8977f7bfcc218c1f2990d472a9b56dd
Georgii Morgunov <georgii.morgunov@mail.ru>
2020-07-31 22:29:15 +0300

    Телефонный справочник на базе ArrayList.

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -2,6 +2,7 @@ package ru.job4j.search;
 
 import java.util.ArrayList;
 
+
 public class PhoneDictionary {
     private ArrayList<Person> persons = new ArrayList<Person>();
 
@@ -11,15 +12,16 @@ public class PhoneDictionary {
 
     /**
      * Вернуть список всех пользователей, который содержат key в любых полях.
-     *
      * @param key Ключ поиска.
      * @return Список подощедщих пользователей.
      */
     public ArrayList<Person> find(String key) {
         ArrayList<Person> result = new ArrayList<>();
-        persons.contains(key);
         for (Person person : persons) {
-            result.add(0, person);
+            if (person.getName().contains(key) || person.getSurname().contains(key) || person.getPhone().contains(key)
+                    || person.getAddress().contains(key)) {
+                result.add(person);
+            }
         }
         return result;
     }
