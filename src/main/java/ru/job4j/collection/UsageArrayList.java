998faa1cf5900fab75d5af79a5153fb85124bb2b
Georgii Morgunov <georgii.morgunov@mail.ru>
2020-07-31 17:41:17 +0300

    Телефонный справочник на базе ArrayList.

--- src/main/java/ru/job4j/collection/UsageArrayList.java
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -11,8 +11,17 @@ public class UsageArrayList {
         names.add("Stepan");
 
         //for (тип и название: название массива/коллекции) {
-        for(String string:names) {
+        for (String string : names) {
             System.out.println(string);
+
+//        String [] deliveryCities = {"Chicago", "New York", "Toronto", "Paris"};
+//        String [] deliveryCities2 = new String[deliveryCities.length + 1];
+//        for (int i = 0; i < deliveryCities.length; i++) {
+//            deliveryCities2[i] = deliveryCities[i];
+//        }
+//
+//        deliveryCities2[deliveryCities.length] = "London";
         }
     }
 }
+
