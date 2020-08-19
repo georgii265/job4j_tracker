9ebaacfb7cc6960afe1b877fdf56e42083debb24
Georgii Morgunov <georgii.morgunov@mail.ru>
2020-08-19 06:38:26 +0300

    Set, HashSet, Iterator.

--- src/main/java/ru/job4j/collection/UsageHashSet.java
+++ src/main/java/ru/job4j/collection/UsageHashSet.java
@@ -3,7 +3,6 @@ package ru.job4j.collection;
 import java.util.HashSet;
 
 public class UsageHashSet {
-
     public static void main(String[] args) {
 
         HashSet<String> autos = new HashSet<String>();
