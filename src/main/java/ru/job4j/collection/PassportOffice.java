dc13f635becd456f46b395b66073e89faaa58c9d
Georgii Morgunov <georgii.morgunov@mail.ru>
2020-08-26 13:56:28 +0300

    Паспорт и жители.

--- src/main/java/ru/job4j/collection/PassportOffice.java
+++ src/main/java/ru/job4j/collection/PassportOffice.java
@@ -10,6 +10,7 @@ public class PassportOffice {
         boolean rsl = false;
         if (!citizens.containsKey(citizen)) {
             citizens.put(citizen.getPassport(), citizen);
+            rsl = true;
         }
         return rsl;
     }
