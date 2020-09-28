6d14d67d5e9742974ecf737b3d72b17f9623a8ad
Georgii Morgunov <georgii.morgunov@mail.ru>
2020-09-28 16:48:13 +0300

    JaCoCo. Процент покрытие тестами.

---
+++ src/main/java/ru/job4j/lesson/Predator.java
@@ -0,0 +1,8 @@
+package ru.job4j.lesson;
+
+
+public class Predator extends Animal {
+    public Predator(String name) {
+        super(name);
+    }
+}
---
+++ src/main/java/ru/job4j/lesson/Tiger.java
@@ -0,0 +1,9 @@
+package ru.job4j.lesson;
+
+
+public class Tiger extends Animal {
+
+    public Tiger(String name) {
+        super(name);
+    }
+}
