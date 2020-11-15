26b846738797526ca860b3393c16f9bf42abc57a
Georgii Morgunov <georgii.morgunov@mail.ru>
2020-11-15 22:53:56 +0300

    Уникальность элементов и сортировка.

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -8,12 +8,7 @@ public class Profiles {
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
                 .map(Profile::getAddress)
-                .sorted(new Comparator<Address>() {
-                    @Override
-                    public int compare(Address o1, Address o2) {
-                        return o1.getCity().compareTo(o2.getCity());
-                    }
-                })
+                .sorted(Comparator.comparing(Address::getCity))
                 .distinct()
                 .collect(Collectors.toList());
     }
