d061fc0aed6c75efcd662bbc954159a7a246a4a8
Georgii Morgunov <georgii.morgunov@mail.ru>
2020-12-17 23:06:26 +0300

    Local-Variable Type Inference.

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -3,7 +3,14 @@ package ru.job4j.search;
 import java.util.ArrayList;
 import java.util.function.Predicate;
 
-
+/**
+ * Вернуть список всех пользователей, который содержат key в любых полях.
+ * @param key Ключ поиска.
+ *
+ * var - это ключевое слово упрощающее создания переменных.
+ *
+ * @return Список подощедщих пользователей.
+ **/
 public class PhoneDictionary {
     private ArrayList<Person> persons = new ArrayList<Person>();
 
@@ -11,19 +18,14 @@ public class PhoneDictionary {
         this.persons.add(person);
     }
 
-    /**
-     * Вернуть список всех пользователей, который содержат key в любых полях.
-     * @param key Ключ поиска.
-     * @return Список подощедщих пользователей.
-     */
     public ArrayList<Person> find(String key) {
         Predicate<Person> name = person -> person.getName().contains(key);
         Predicate<Person> surname = person -> person.getSurname().contains(key);
         Predicate<Person> phone = person -> person.getPhone().contains(key);
         Predicate<Person> address = person -> person.getAddress().contains(key);
-        Predicate<Person> combine = name.or(surname.or(phone.or(address)));
+        var combine = name.or(surname.or(phone.or(address)));
         ArrayList<Person> result = new ArrayList<>();
-        for (Person person : persons) {
+        for (var person : persons) {
             if (combine.test(person)) {
                 result.add(person);
             }
--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -2,20 +2,19 @@ package ru.job4j.search;
 
 import java.util.LinkedList;
 
+/**
+ * Метод должен вставлять в нужную позицию элемент.
+ * Позиция определяется по полю приоритет.
+ * Для вставки использовать add(int index, E value)
+ * @param task задача
+ */
 public class PriorityQueue {
     private LinkedList<Task> tasks = new LinkedList<>();
 
-    /**
-     * Метод должен вставлять в нужную позицию элемент.
-     * Позиция определяется по полю приоритет.
-     * Для вставки использовать add(int index, E value)
-     *
-     * @param task задача
-     */
     public void put(Task task) {
-        int index = 0;
+        var index = 0;
 
-        for (Task element : tasks) {
+        for (var element : tasks) {
             if (element.getPriority() > task.getPriority()) {
                 break;
             }
